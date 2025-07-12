de6189f53a62dcddfa3f458d9723f4b7acbe91fe
masterShashi <shashikumargupta443@gmail.com>
2025-07-12 12:07:22 +0530

    add middlware update profile safety

--- backend/routes/profileRoute.js
+++ backend/routes/profileRoute.js
@@ -3,6 +3,6 @@ const { getProfile, updateProfile } = require('../controllers/profile');
 const profileRouter = require('express').Router();
 
 profileRouter.get('/getProfile/:id', getProfile);
-profileRouter.post('/updateProfile/:id', updateProfile);
+profileRouter.post('/updateProfile/:id', authMiddleware, updateProfile);
 
 module.exports = profileRouter;
